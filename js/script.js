d0c784040efde3e7335fbf39c2cb7183187015c5
Franco Sarachu <fsarachu@gmail.com>
2016-12-19 06:54:23 -0300

    Reformat files

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
 function loadData() {
 
-  var $body = $('body');
-  var $wikiElem = $('#wikipedia-links');
-  var $nytHeaderElem = $('#nytimes-header');
-  var $nytElem = $('#nytimes-articles');
-  var $greeting = $('#greeting');
+    var $body = $('body');
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var $greeting = $('#greeting');
 
-  // clear out old data before new request
-  $wikiElem.text("");
-  $nytElem.text("");
+    // clear out old data before new request
+    $wikiElem.text("");
+    $nytElem.text("");
 
-  // load streetview
-  var $bgImg = $('<img src="" alt="" width="100%" class="bgimg">');
-  $body.append($bgImg);
+    // load streetview
+    var $bgImg = $('<img src="" alt="" width="100%" class="bgimg">');
+    $body.append($bgImg);
 
-  var street = $('#street').val();
-  var city = $('#city').val();
+    var street = $('#street').val();
+    var city = $('#city').val();
 
-  if (city && street) {
-    $bgImg.attr('src', 'http://maps.googleapis.com/maps/api/streetview?key=AIzaSyB3PKBgLPyYLiMm1lgRBlxz59fNkxcaHrw&size=600x400&location=' + street + ', ' + city);
-  }
+    if (city && street) {
+        $bgImg.attr('src', 'http://maps.googleapis.com/maps/api/streetview?key=AIzaSyB3PKBgLPyYLiMm1lgRBlxz59fNkxcaHrw&size=600x400&location=' + street + ', ' + city);
+    }
 
-  return false;
+    return false;
 }
 
 $('#form-container').submit(loadData).submit();
