31db8fd768196b7dd25a7bc0dd1898f5b88adbf3
Franco Sarachu <fsarachu@gmail.com>
2016-12-19 05:35:01 -0300

    Reformat files

--- js/script.js
+++ js/script.js
@@ -1,21 +1,20 @@
-
 function loadData() {
 
-    var $body = $('body');
-    var $wikiElem = $('#wikipedia-links');
-    var $nytHeaderElem = $('#nytimes-header');
-    var $nytElem = $('#nytimes-articles');
-    var $greeting = $('#greeting');
+  var $body = $('body');
+  var $wikiElem = $('#wikipedia-links');
+  var $nytHeaderElem = $('#nytimes-header');
+  var $nytElem = $('#nytimes-articles');
+  var $greeting = $('#greeting');
 
-    // clear out old data before new request
-    $wikiElem.text("");
-    $nytElem.text("");
+  // clear out old data before new request
+  $wikiElem.text("");
+  $nytElem.text("");
 
-    // load streetview
+  // load streetview
 
-    // YOUR CODE GOES HERE!
+  // YOUR CODE GOES HERE!
 
-    return false;
+  return false;
 };
 
 $('#form-container').submit(loadData);
